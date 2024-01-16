6e9819efe8a4dbf860e442fa79babe2204030b73
Fenar Khan <fenarkhan@gmail.com>
2024-01-16 23:53:41 +0530

    jlksxfjcskdfj

--- React/chapter3/my-first-react-app/src/App.js
+++ React/chapter3/my-first-react-app/src/App.js
@@ -1,20 +1,26 @@
-import './App.css'
-import {useState} from 'react'
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [name,setName] = useState('mario')
-
-  const handleClick = () =>{
-    setName('luigi')
-    console.log('name changed'+name)
-  }
-
   return (
     <div className="App">
-      <h1>My name is {name}</h1>
-      <button onClick={handleClick}>Change Name</button>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React & Firebase
+        </a>
+      </header>
     </div>
   );
 }
 
 export default App;
+//dfsdfs
